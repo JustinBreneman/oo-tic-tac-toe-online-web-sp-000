58f0e62a6572d4ac0f9c4edba706773cdb89aa52
Justin Breneman <justin.breneman@yahoo.com>
2020-06-12 23:19:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,12 +87,13 @@ class TicTacToe
       position_3 = @board[win_index_3]
 
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
-        win_combination
+        win_combo = win_combination
       elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-        win_combination
+        win_combo = win_combination
       else
         false
       end
     end
+    win_combo
   end
 end
